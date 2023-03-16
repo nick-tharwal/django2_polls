bf690e98e1f27917c3c56ccce2d24e24e53d21d8
nick-tharwal <sharmanikhil6922@gmail.com>
2023-03-16 19:29:27 +0530

    fourth

--- polls/poll/views.py
+++ polls/poll/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , get_object_or_404
+from django.http import HttpResponse , Http404
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello world you're at poll index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    
+    context = { 'latest_question_list' : latest_question_list }
+    return render(request, 'poll/index.html',context)
+    
+   
+
+def detail(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+
+    return render(request , 'poll/detail.html',{'question': question })
+
+def results(request,question_id):
+    return HttpResponse("these are the results of the question %s " % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting page of Question %s" % question_id)
+
 
 # Create your views here.
--- polls/polls/urls.py
+++ polls/polls/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('',include('poll.urls')),
-    path('polls/',include('poll.urls')),
+    
+    path('poll/',include('poll.urls')),
     path('admin/', admin.site.urls),
 ]
