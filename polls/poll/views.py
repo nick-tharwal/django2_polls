4f4752ef15cb321e6ce83ba967cf755ac5e98dc1
nick-tharwal <sharmanikhil6922@gmail.com>
2023-03-18 00:27:15 +0530

    four

--- polls/poll/views.py
+++ polls/poll/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , get_object_or_404
-from django.http import HttpResponse , Http404
-from .models import Question
+from django.http import HttpResponse , Http404 , HttpResponseRedirect
+from .models import Question , Choice
 
 
 def index(request):
@@ -17,10 +17,22 @@ def detail(request, question_id):
     return render(request , 'poll/detail.html',{'question': question })
 
 def results(request,question_id):
-    return HttpResponse("these are the results of the question %s " % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'poll/results.html',{ 'question':question })
 
 def vote(request, question_id):
-    return HttpResponse("Voting page of Question %s" % question_id)
+    question = get_object_or_404(Question , pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError , Choice.DoesNotExist):
+        return render(request,'poll/detail.html',{
+            'question' : question,
+            'error_messege' : "You didn't select a choice"
+        })
 
+    else:
+        selected_choice.votes += 1
+        selected_choice.save() 
+    return HttpResponseRedirect(reverse('poll:results',args=(question.id,)))
 
 # Create your views here.
